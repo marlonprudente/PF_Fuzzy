5ea78652351a5acc1429aa856e3a0aacd8938979
Usuário <Usurio@DESKTOP-U959U4T>
2017-11-08 10:39:26 -0200

    Testando API jFuzzyLogic http://jfuzzylogic.sourceforge.net/html/manual.html

--- FuzzyApp/src/Main.java
+++ FuzzyApp/src/Main.java
@@ -1,6 +1,3 @@
-
-import net.sourceforge.jFuzzyLogic.fcl.*;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,14 +6,44 @@ import net.sourceforge.jFuzzyLogic.fcl.*;
 
 /**
  *
- * @author Usuário
+ * @author Marlon Prudente
+ */
+import net.sourceforge.jFuzzyLogic.*;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.*;
+//import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
+
+/**
+ * Test parsing an FCL file
+ * @author pcingola@users.sourceforge.net
  */
 public class Main {
-    public static void main(String[] args) {
-        //System.out.println("Hello.");
-        
+    public static void main(String[] args) throws Exception {
+        // Load from 'FCL' file
+        String fileName = "src/tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
 
-        
-        
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        // Show 
+        JFuzzyChart.get().chart(fis);
+
+        // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable tip = fis.getVariable("tip");
+        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Print ruleSet
+        System.out.println(fis);
     }
 }
