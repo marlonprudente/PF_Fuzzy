f6ab211469bfd011fe4c999bf6ef655406769cbb
Usuário <Usurio@DESKTOP-U959U4T>
2017-11-08 09:31:18 -0200

    Testando API jFuzzyLogic http://jfuzzylogic.sourceforge.net/html/manual.html

--- FuzzyApp/src/Main.java
+++ FuzzyApp/src/Main.java
@@ -1,3 +1,6 @@
+
+import net.sourceforge.jFuzzyLogic.fcl.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,10 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello.");
+        //System.out.println("Hello.");
+        
+
+        
+        
     }
 }
